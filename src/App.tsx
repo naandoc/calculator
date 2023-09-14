3b05288c78886ec2edba190bf001514e015d67d1
Nando Cavalcanti <naandoc887477@gmail.com>
2023-09-14 17:06:34 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { type ReactElement } from 'react';
+import './App.css';
+
+function App(): ReactElement {
+    return (
+        <>
+            <h1>Calculator</h1>
+        </>
+    );
+}
+
+export default App;
