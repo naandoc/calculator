6447629b68e627ffb3e7e31c60abce81bf49d7ea
Nando Cavalcanti <naandoc887477@gmail.com>
2023-09-16 13:12:55 -0300

    finalização do componente de digitação de números do usuário

--- src/components/digitation-field/DigitationField.1.tsx
+++
@@ -1,3 +0,0 @@
-export const DigitationField = (): Element => {
-  return <div>DigitationField</div>;
-};
--- src/components/digitation-field/DigitationField.tsx
+++ src/components/digitation-field/DigitationField.tsx
@@ -1,10 +1,31 @@
-import { type ReactElement } from 'react';
+import { type ReactElement, useState } from 'react';
 import './DigitationField.css';
 
 const DigitationField = (): ReactElement => {
+  // state that will monitor the value entered by the user
+  const [valueInput, setValueInput] = useState('');
+
+  // function that checks what the user typed
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    // if you entered a string of numbers,
+    // it is converted to number, otherwise it only returns
+    const valueIsNumber = e.target.value;
+
+    if (isNaN(Number(valueIsNumber))) return;
+
+    setValueInput(valueIsNumber);
+
+    console.log(valueIsNumber);
+  };
+
   return (
-    <div>
-      <input type="text" value={0} />
+    <div className="typing-field">
+      <input
+        onChange={handleInputChange}
+        type="text"
+        value={valueInput}
+        placeholder="0"
+      />
     </div>
   );
 };
