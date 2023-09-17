5653c172b83b4b22ae7096ab13607b36ae4d13f7
Nando Cavalcanti <naandoc887477@gmail.com>
2023-09-17 09:13:48 -0300

    finalização do design da calculadora

--- src/components/numeric-keyboard/NumericKeyboard.tsx
+++ src/components/numeric-keyboard/NumericKeyboard.tsx
@@ -1,5 +1,6 @@
 import { type ReactElement } from 'react';
 import Buttom from '../buttons/Buttom';
+import './NumericKeyboard.css';
 
 const NumericKeyboard = (): ReactElement => {
   const handleEventDigit = (e: React.MouseEvent): void => {
@@ -9,15 +10,32 @@ const NumericKeyboard = (): ReactElement => {
   };
 
   return (
-    <div>
+    <div className="key-board">
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom="(" />
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom=")" />
+
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom="%" />
       <Buttom
         digitEvent={handleEventDigit}
         colorBg="bgWhite"
-        digitButtom="10"
+        digitButtom="AC"
       />
-      {/* <Buttom colorBg="bgWhite" digitButtom="10" />
-      <Buttom colorBg="" digitButtom="5" />
-      <Buttom colorBg="" digitButtom="25" /> */}
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="7" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="8" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="9" />
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom="/" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="4" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="5" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="6" />
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom="x" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="1" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="2" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="3" />
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom="-" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="0" />
+      <Buttom digitEvent={handleEventDigit} colorBg="" digitButtom="." />
+      <Buttom digitEvent={handleEventDigit} colorBg="bgBlue" digitButtom="=" />
+      <Buttom digitEvent={handleEventDigit} colorBg="bgWhite" digitButtom="+" />
     </div>
   );
 };
